71176eee6245064bbe74622c906c5827b5370066
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2022-07-22 20:50:49 -0700

    finished edit a spot endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -79,6 +79,56 @@ const validateSpotCreation = [
   handleValidationErrors
 ];
 
+const validateSpotEdit = [
+  check('address')
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('Street address is required'),
+  check('city')
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('City is required'),
+  check('state')
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('State is required'),
+  check('country')
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('Country is required'),
+  check('lat')
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('Latitude is not valid'),
+  check('lng')
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('Longitude is not valid'),
+  check('name')
+  .exists({
+    checkFalsy: true
+  })
+  .custom((value, { req }) => value.length < 50)
+  .withMessage('Name must be less than 50 characters'),
+  check('description')
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('Description is required'),
+  check('price')
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('Price per day is required'),
+  handleValidationErrors
+];
+
 // Return all spots
 router.get(
   '/',
@@ -175,7 +225,7 @@ router.post(
   '/',
   [requireAuth, validateSpotCreation],
   async (req, res) => {
-    // console.log(req.user.id)
+
     const {
       address,
       city,
@@ -213,7 +263,7 @@ router.post(
 // Edit a spot
 router.put(
   '/:spotId',
-  [requireAuth],
+  [requireAuth, validateSpotEdit],
   async (req, res) => {
     // console.log(req.user.id)
     const {
@@ -230,7 +280,17 @@ router.put(
 
     const spotId = Number(req.params.spotId);
 
-    const spot = await Spot.findByPk(spotId);
+    const spot = await Spot.findByPk(spotId, {
+      attributes: {
+        exclude: ['previewImage']
+      }
+    });
+
+    if (!spot) {
+      let err = new Error("Spot couldn't be found");
+      err.status = 404;
+      throw err;
+    }
 
     const updatedSpot = await spot.update({
       address,
@@ -246,8 +306,6 @@ router.put(
 
     res.status(200);
     res.json(updatedSpot);
-
-
   }
 );
 
