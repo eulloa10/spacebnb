f80659280fbc28ebe74c6f19b0c1f6ee0a56b0a8
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2022-12-14 19:51:16 -0800

    updated Render.com schema

--- backend/db/seeders/20220719064022-images.js
+++ backend/db/seeders/20220719064022-images.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName = 'Images';
-     await queryInterface.bulkInsert('Images', [
+     await queryInterface.bulkInsert(options, [
       {
        imageableId: 1,
        imageableType: 'spot',
